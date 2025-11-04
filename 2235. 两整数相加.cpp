7170d12ef255b3a87cc428d838d7a9eb36297424
qhe32673-arch <qhe32673@gmail.com>
2025-11-04 11:40:49 +0800

    数学

---
+++ 2235. 两整数相加.cpp
@@ -0,0 +1,15 @@
+// 思路
+// 1.return num1+num2;//返回和
+// Code
+
+// 作者：StellarVibes
+// 链接：https://leetcode.cn/problems/add-two-integers/solutions/3823043/2235-liang-zheng-shu-xiang-jia-by-stella-9utj/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;//返回和
+    }
+};
